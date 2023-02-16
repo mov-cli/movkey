ac236fef0e9b1fa55825ab430f5a2d665459064d
Ananas <83706294+R3tr0Ananas@users.noreply.github.com>
2023-02-16 21:43:55 +0100

    Update main.py

--- .github/main.py
+++ .github/main.py
@@ -7,12 +7,20 @@ import click
 def key(key):
     dokicloud = httpx.get("https://api.github.com/repos/consumet/rapidclown/commits/dokicloud", headers={"Authorization": f"Bearer {key}"}).json()
     rabbitstream = httpx.get("https://api.github.com/repos/consumet/rapidclown/commits/rabbitstream", headers={"Authorization": f"Bearer {key}"}).json()
+    enimax = httpx.get("https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt", headers={"Authorization": f"Bearer {key}"}).json()
     dokidate = dokicloud["commit"]["author"]["date"]
     rabbitdate = rabbitstream["commit"]["author"]["date"]
+    enidate = enimax["commit"]["author"]["date"]
     if dokidate > rabbitdate:
-        decryptkey = "https://raw.githubusercontent.com/consumet/rapidclown/dokicloud/key.txt"
+        if dokidate > enidate:
+            decryptkey = "https://raw.githubusercontent.com/consumet/rapidclown/dokicloud/key.txt"
+        else:
+            decryptkey = "https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt"
     else:
-        decryptkey = "https://raw.githubusercontent.com/consumet/rapidclown/rabbitstream/key.txt"
+        if rabbitdate > enidate:  
+            decryptkey = "https://raw.githubusercontent.com/consumet/rapidclown/rabbitstream/key.txt"
+        else:
+            decryptkey = "https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt"
     u = httpx.get(decryptkey).text
     with open(f"key.txt", "w") as f:
         f.write(u)
